80702c7935b575088b956ba8373d665d7a58d4e5
Chris DeLashmutt <cdelashmutt@pivotal.io>
2016-09-20 17:32:09 -0400

    Add project files.

---
+++ EncryptXmlExample/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(EncryptXmlExample.Startup))]
+namespace EncryptXmlExample
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
